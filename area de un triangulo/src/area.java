038199e0b048ca28c4de65d677f0a13f5165fc52
Jhon-D15 <149914828+Jhon-D15@users.noreply.github.com>
2023-11-16 00:41:17 -0500

    area

---
+++ area de un triangulo/src/area.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class area {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        double base;
+        double altura;
+        double area;
+
+        System.out.println("ingresar el valor de la base");
+        base= lector.nextDouble();
+
+        System.out.println("ingresar el valor de la altura");
+        altura= lector.nextDouble();
+
+        area = (base * altura)/2;
+
+        System.out.println("el area del triangulo es:"+" "+area);
+
+    }
+}
